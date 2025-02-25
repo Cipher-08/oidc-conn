7ff5716fa6220f577bdf8bf6eb919fa8a407dde5
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-02-25 12:34:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" 
 }
 
-resource "aws_s3_bucket" "backup" {
-  bucket = "prod-backup"
+resource "aws_s3_bucket" "prod_backup" {
+  bucket = "prod-backup-2025" 
 
   tags = {
-    Name        = "prod-backup"
+    Name        = "prod-backup-2025"
     Environment = "Production"
   }
 }
 
-resource "aws_s3_bucket_versioning" "backup_versioning" {
-  bucket = aws_s3_bucket.backup.id
+resource "aws_s3_bucket_versioning" "prod_backup_versioning" {
+  bucket = aws_s3_bucket.prod_backup.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "backup_encryption" {
-  bucket = aws_s3_bucket.backup.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "prod_backup_encryption" {
+  bucket = aws_s3_bucket.prod_backup.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -29,5 +29,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backup_encryption
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.backup.bucket
+  value = aws_s3_bucket.prod_backup.bucket
 }
